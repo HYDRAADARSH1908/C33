c5adfa25a6506dc8fcada94b3608ab9fa4bf0e1f
HYDRAADARSH1908 <85999787+HYDRAADARSH1908@users.noreply.github.com>
2021-08-14 18:41:59 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,26 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visibility= 255
+  }
+display()
+{
+  console.log(this.body.speed)
+    
+    if( this.body.speed> 3){
+        
+      World.remove(world, this.body)
+      push ()
+      tint (255, this.visibility)
+      super.display()
+      this.visibility-=5
+       pop ()
+    }
+      else{
+      
+        super.display()
+    }
+
+  }
+};
